87dd55c8d6728ec89a9ecd7c8602980ea3821261
tediscript <tediscript@gmail.com>
2010-10-20 23:15:16 +0700

    bug fix add and edit for label

--- system/application/views/view_add_template.php
+++ system/application/views/view_add_template.php
@@ -5,8 +5,8 @@
         <title></title>
     </head>
     <body>
-        <form action="{php_open} echo site_url() . '/{table_name}/add' {php_close}" method="POST">
-            Nama:<br/>{table_fields_view_add}
+        <form action="{php_open} echo site_url() . '/{table_name}/add' {php_close}" method="POST">{table_fields_view_add}
+            {table_field_humanized}:<br/>
             <input type="text" name="{table_field}" value="{php_open} echo set_value('{table_field}'); {php_close}" /><br/>{/table_fields_view_add}
             <input type="submit" value="Add" />
         </form>
--- system/application/views/view_edit_template.php
+++ system/application/views/view_edit_template.php
@@ -5,8 +5,8 @@
         <title></title>
     </head>
     <body>
-        <form action="{php_open} echo site_url() . '/{table_name}/edit/' . ${table_name}['{primary_key}']; {php_close}" method="POST">
-            Nama:<br/>{table_fields_view_edit}
+        <form action="{php_open} echo site_url() . '/{table_name}/edit/' . ${table_name}['{primary_key}']; {php_close}" method="POST">{table_fields_view_edit}
+            {table_field_humanized}:<br/>
             <input type="text" name="{table_field}" value="{php_open} echo set_value('{table_field}', ${table_name}['{table_field}']); {php_close}" /><br/>{/table_fields_view_edit}
             <input type="submit" value="Edit" />
         </form>
